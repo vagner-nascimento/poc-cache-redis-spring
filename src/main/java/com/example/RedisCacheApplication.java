e8ce49298d41b5de528d8220fe27413938446a89
Vagner Nascimento <vagnernasc88@gmail.com>
2018-12-12 23:58:09 -0200

    Initial commit

---
+++ src/main/java/com/example/RedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class RedisCacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCacheApplication.class, args);
+    }
+}
